545cb36ff95b3cca86e7342dd880141f929dae37
RusssLee <ruslee.play@gmail.com>
2025-08-28 13:23:36 +0300

    Initial Commit

---
+++ weather_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather_practice
+//
+//  Created by Ruslan Popovich on 28/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weather_practiceTests/weather_practiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_practiceTests.swift
+//  weather_practiceTests
+//
+//  Created by Ruslan Popovich on 28/08/2025.
+//
+
+import Testing
+@testable import weather_practice
+
+struct weather_practiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
